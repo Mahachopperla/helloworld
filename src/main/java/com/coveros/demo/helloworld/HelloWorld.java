187312c9f5559f0e71e43e325cfbe5a6a210205b
Mahalakshmi Chopperla <109203613+Mahachopperla@users.noreply.github.com>
2025-01-03 17:09:36 +0530

    Update HelloWorld.java

--- src/main/java/com/coveros/demo/helloworld/HelloWorld.java
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -1,15 +1,25 @@
 package com.coveros.demo.helloworld;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class HelloWorld {
+@WebServlet("/helloworld")  // Mapping the servlet to /helloworld
+public class HelloWorld extends javax.servlet.http.HttpServlet {
 
-  public static void main(final String[] args) {
-    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
-    final LocalDateTime now = LocalDateTime.now();
+    @Override
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws ServletException, IOException {
+        // Set the content type to HTML
+        response.setContentType("text/html");
 
-    System.out.println("Hello, World! The current time is " + dtf.format(now));
-  }
+        // Get the PrintWriter to send content back to the client
+        PrintWriter out = response.getWriter();
 
+        // Output the message
+        out.println("<html><body>");
+        out.println("<h1>Hello, World!</h1>");
+        out.println("</body></html>");
+    }
 }
+
